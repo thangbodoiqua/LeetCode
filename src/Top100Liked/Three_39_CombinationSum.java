fe60fff37af02c9a2af6a4554f6585c729f28b0c
thangbodoiqua <dluy2406@gmail.com>
2024-11-27 00:09:34 +0700

    day1_three_39CombinationSum

---
+++ src/Top100Liked/Three_39_CombinationSum.java
@@ -0,0 +1,47 @@
+package Top100Liked;
+import java.util.*;
+public class Three_39_CombinationSum {
+    public static void main(String[] args) {
+        int[] candidates = {8,7,4,3};
+        System.out.println(combinationSum(candidates, 11));
+    }
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> sums = new ArrayList<>();
+        backtrack(sums, candidates, target, new ArrayList<>());
+        return sums;
+    }
+
+    public static void backtrack(List<List<Integer>> sums, int[] candidates, int tagret, List<Integer> total){
+        if(calSum(total) == tagret){
+            sums.add(new ArrayList<>(total));
+            return;
+        }
+
+        if (calSum(total) > tagret){
+            return;
+        }
+
+        int i = 0;
+
+        if (!total.isEmpty()){
+            for (int j = 0; j < candidates.length; j++){
+                if (candidates[j] == total.getLast()){
+                    i = j;
+                }
+            }
+        }
+
+        for (;i < candidates.length; i++){
+            total.add(candidates[i]);
+            backtrack(sums, candidates, tagret, total);
+            total.removeLast();
+        }
+    }
+    public static int calSum(List<Integer> total){
+        int sum = 0;
+        for (int i = 0; i < total.size(); i++){
+            sum += total.get(i);
+        }
+        return sum;
+    }
+}
