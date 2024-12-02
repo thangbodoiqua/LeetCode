8f9fca7486838c3ff52062190f04f58333128d69
thangbodoiqua <dluy2406@gmail.com>
2024-12-02 13:01:18 +0700

    day3_sixth_35_SearchInsertPos

--- src/Top100Liked/Fifth_78_Subsets.java
+++ src/Top100Liked/Fifth_78_Subsets.java
@@ -1,9 +1,10 @@
 package Top100Liked;
-import java.lang.reflect.Array;
 import java.util.*;
-public class fifth_78_Subsets {
+
+public class Fifth_78_Subsets {
+    // https://leetcode.com/problems/subsets/solutions/6081850/simple-solution-with-diagrams-in-video-javascript-c-java-python/?envType=study-plan-v2&envId=top-100-liked
     public static void main(String[] args) {
-        System.out.println(subsets(new int[]{3,2,1}));
+        System.out.println(subsets(new int[]{4,7,1,2}));
     }
     public static  List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> subsets = new ArrayList<>();
@@ -11,7 +12,8 @@ public class fifth_78_Subsets {
         return subsets;
     }
     public static void backtrack(List<List<Integer>> subsets, int[] nums, List<Integer> sub) {
-        subsets.add(new ArrayList<>(sub));
+        if (!subsets.contains(sub))
+            subsets.add(new ArrayList<>(sub));
         for (int i = 0; i < nums.length; i++){
             if (sub.size() > 0 && sub.getLast() >= nums[i]) {
                 continue;
---
+++ src/Top100Liked/Sixth_35_SearchInsertPos.java
@@ -0,0 +1,26 @@
+package Top100Liked;
+
+import java.util.Arrays;
+
+public class Sixth_35_SearchInsertPos {
+    public static void main(String[] args) {
+        int[] nums = {2,5,6,7};
+        System.out.println(searchInsert(nums, 7));
+    }
+    public static int searchInsert(int[] nums, int target){
+        return binarySearch(nums, target, 0, nums.length);
+    }
+    public static int binarySearch(int[] nums, int target, int from, int to){
+        int mid = (to+from)/2;
+        if (mid == to || nums[mid] == target){
+            return mid;
+        }else {
+            if (nums[mid] > target) {
+                return binarySearch(nums, target, from, mid - 1);
+            }else {
+                return binarySearch(nums, target, mid+1, to);
+            }
+
+        }
+    }
+}
