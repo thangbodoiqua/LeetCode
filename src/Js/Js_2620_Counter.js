1575ffd1bd2f0a96420e6ab36650b4aa8715b92b
Thangbodoiqua <dluy2406@gmail.com>
2025-01-13 22:36:33 +0700

    Js_2620_Counter add explain code

--- src/Js/Js_2620_Counter.js
+++ src/Js/Js_2620_Counter.js
@@ -4,7 +4,7 @@ and then returns 1 more than the previous
 value every subsequent time it is called (n, n + 1, n + 2, etc).
  */
 var createCounter = function (n) {
-    return function counter() {
+    return function count() {
         return n++;
     };
 };
@@ -13,3 +13,8 @@ const counter = createCounter(10);
 console.log(counter());
 console.log(counter());
 console.log(counter());
+
+//Thực chất đoạn code này khá khó hiểu ở việc tạo ra và gọi hàm liên tục thì nó lại vẫn lưu đc giá trị của n
+//Nhưng hiểu một cách đơn giản hơn, ta  coi  createCounter bên trong kia là 1 constructor, n là một thuộc tính của lớp
+// Lúc này ta tạo ra biến counter tức cũng là tạo một đối tượng của lớp creatCounter
+// Và với mỗi lần gọi counter ra là ta đang gọi hàm thực hiện việc tăng thuộc tính n lên một giá trị
